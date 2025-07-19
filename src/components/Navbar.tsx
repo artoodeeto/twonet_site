3fb13f0585c995b63b5b02032b167a8250118f1f
raphael gako <raphaelg2012@gmail.com>
2025-07-18 17:44:00 -0700

    fixed, removed next link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -108,12 +108,12 @@ export const Navbar = () => {
           <ul className="items-center justify-end flex-1 pt-6 list-none lg:pt-0 lg:flex">
             {navigation.map((menu, index) => (
               <li className="mr-3 nav__item" key={index}>
-                <Link
+                <a
                   href={`${menu.href}`}
                   className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-200 hover:text-blue-500 focus:text-blue-500 focus:bg-blue-100 focus:outline-none dark:focus:bg-gray-800"
                 >
                   {menu.label}
-                </Link>
+                </a>
               </li>
             ))}
           </ul>
